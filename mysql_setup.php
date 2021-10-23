a1cbc8a5333a721c26080a25ed85dfd68432d641
Paul <paul.ok.98@gmail.com>
2021-10-23 14:11:07 -0400

    db setup

---
+++ mysql_setup.php
@@ -0,0 +1,36 @@
+<?php
+
+    /* Setup */
+    include('database_connection.php');
+    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+    $db = new mysqli($dbserver, $dbuser, $dbpass, $dbdatabase);
+
+    /* Setting up user table */
+    $db->query("drop table if exists user");
+    $db->query("create table user (
+        uid int not null auto_increment,
+        name text not null,
+        email text not null,
+        password text not null,
+        primary key (uid));");
+
+    /* Setting up location table */
+    $db->query("drop table if exists location");
+    $db->query("create table location (
+        lid int not null auto_increment,
+        name text not null,
+        address text not null,
+        uid int not null,
+        inout varchar(255) default "out",
+        time varchar(255) default "night",
+        money varch(255) default "$$$",
+        activity varchar(255) default "other",
+        primary key (lid));");
+
+    /* Setting up picture table */
+    $db->query("drop table if exists picture");
+    $db->query("create table picture (
+        pid int not null auto_increment,
+        uid int not null,
+        name text not null,
+        description text not null);");
