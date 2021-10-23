6f0100d2073292372753f5105295d859c03b35a9
Bryan Kim <bryankim13173@gmail.com>
2021-10-23 14:34:02 -0400

    edited login and signup

--- signup.php
+++ signup.php
@@ -37,22 +37,37 @@ if (isset($_POST["email"])) { /// validate the email coming in
 }
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
     <head>
-        <meta charset="UTF-8">  
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="author" content="your name">
-        <meta name="description" content="include some description about your page">  
-        <title>Trivia Game Login</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous"> 
-    </head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1"> 
+        <title>KaClik!</title>
+        <meta name="author" content="Bryan Kim : bjk3yf worked on gallery page and home page, Paul Ok : pso3td worked on suggestion page and home page">
+        <meta name="description" content="Providing potential photoshoot locations to users">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+        <meta name="keywords" content="Photos, camera, location, suggestion, scenery">        
+        <link rel="stylesheet" href="styles/main.css">
+        <link rel="stylesheet" href="styles/home.css">
+
+    </head>  
     <body>
-        <div class="container" style="margin-top: 15px;">
-            <div class="row col-xs-8">
-                <h1>CS4640 Television Trivia Game - Get Started</h1>
-                <p> Welcome to our trivia game!  To get started, login below or enter a new username and password to create an account</p>
-            </div>
-            <div class="row justify-content-center">
+        <header>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light border">
+              <a class="navbar-brand px-3 mx-auto" href="index.html">KaClik!</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse justify-content-between"  id="navbarNavAltMarkup">
+                <div class="navbar-nav mx-auto">
+                  <a class="nav-item nav-link active" href="gallery.html">Gallery</a>
+                  <a class="nav-item nav-link active" href="locations.html">Locations</a>
+                  <a class="nav-item nav-link active" href="suggestions.html">Suggestions</a>
+                </div>
+              </div>
+            </nav> 
+        </header>
+        <div class="row justify-content-center">
                 <div class="col-4">
                 <?php
                     if (!empty($error_msg)) {
@@ -79,8 +94,8 @@ if (isset($_POST["email"])) { /// validate the email coming in
                 </form>
                 </div>
             </div>
-        </div>
+        
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
     </body>
 </html>
