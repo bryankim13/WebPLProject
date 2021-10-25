5ed3c4f311e234494e40053ddb0ae65275346bfd
Bryan Kim <bryankim13173@gmail.com>
2021-10-25 13:03:53 -0400

    required fields for login and signup

--- signup.php
+++ signup.php
@@ -94,15 +94,15 @@ if (isset($_POST["email"])) { /// validate the email coming in
                 <form action="signup.php" method="post">
                     <div class="mb-3">
                         <label for="email" class="form-label">Email</label>
-                        <input type="email" class="form-control" id="email" name="email"/>
+                        <input type="email" class="form-control" id="email" name="email" required/>
                     </div>
                     <div class="mb-3">
                         <label for="name" class="form-label">Name</label>
-                        <input type="text" class="form-control" id="name" name="name"/>
+                        <input type="text" class="form-control" id="name" name="name" required/>
                     </div>
                     <div class="mb-3">
                         <label for="password" class="form-label">Password</label>
-                        <input type="password" class="form-control" id="password" name="password"/>
+                        <input type="password" class="form-control" id="password" name="password" required/>
                     </div>
                     <div class="text-center">                
                     <button type="submit" class="btn btn-primary">Create Account</button>
