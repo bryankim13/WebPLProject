4f92b1a182bbcd6a03e3a38332416f12ba5e2163
Bryan Kim <bryankim13173@gmail.com>
2021-12-01 19:56:46 -0500

    finished frontend form

--- angComp/src/app/wishlist/wishlist.component.ts
+++ angComp/src/app/wishlist/wishlist.component.ts
@@ -8,12 +8,22 @@ import { Camera } from '../camera';
 export class WishlistComponent implements OnInit {
 
   camera: Camera;
-
+  confirmationMsg: string;
+  brandNames: Array<string> = ["Sony","Fujifilm","Nikon","Canon","GoPro","Panasonic"];
+  wishes: Array<Camera> = [];
   constructor() { 
     this.camera = new Camera("Sony", "A7", "22mm",25);
+    this.confirmationMsg = "";
   }
 
+  addWish(): void {
+    this.confirmationMsg = "Added to your Wishlist!";
+  }
+  submitForm(data:any): void{
+    let cam = new Camera(data.brand,data.model,data.lens,data.megaPix);
+  }
   ngOnInit(): void {
   }
+  
 
 }
