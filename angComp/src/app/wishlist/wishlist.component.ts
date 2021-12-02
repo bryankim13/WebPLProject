3a9fded41e49b60ad2db323523ab89ddb0ff6a46
Bryan Kim <bryankim13173@gmail.com>
2021-12-01 21:48:10 -0500

    php connection working

--- angComp/src/app/wishlist/wishlist.component.ts
+++ angComp/src/app/wishlist/wishlist.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Camera } from '../camera';
+import {HttpClient, HttpErrorResponse,HttpParams } from '@angular/common/http';
+import { subscribeOn } from 'rxjs';
 @Component({
   selector: 'app-wishlist',
   templateUrl: './wishlist.component.html',
@@ -9,10 +11,10 @@ export class WishlistComponent implements OnInit {
 
   camera: Camera;
   confirmationMsg: string;
-  brandNames: Array<string> = ["Sony","Fujifilm","Nikon","Canon","GoPro","Panasonic"];
+  brandNames: Array<string> = ["Sony","Nikon","Fujifilm","Canon","Panasonic","GoPro"];
   wishes: Array<Camera> = [];
-  constructor() { 
-    this.camera = new Camera("Sony", "A7", "22mm",25);
+  constructor( private http:HttpClient ) { 
+    this.camera = new Camera("", "", "",0);
     this.confirmationMsg = "";
   }
 
@@ -20,7 +22,16 @@ export class WishlistComponent implements OnInit {
     this.confirmationMsg = "Added to your Wishlist!";
   }
   submitForm(data:any): void{
-    let cam = new Camera(data.brand,data.model,data.lens,data.megaPix);
+    let cam = new Camera(data.brand,data.model,data.lens,data.megapix);
+    this.wishes.push(cam);
+  }
+  response:any;
+  sendWishes():void{
+    let json:string = JSON.stringify(this.wishes);
+    this.http.post<any>("https://cs4640.cs.virginia.edu/bjk3yf/project/wishes.php", json).subscribe(
+      (respData) => { this.response = respData},
+      (error) => {console.log("Error: ",error);}
+    );
   }
   ngOnInit(): void {
   }
